3d6aa27a0bac47a24c9a440903653b0fb73f4c1a
songhh1 <songhh1@yonyou.com>
2015-01-07 11:36:00 +0800

    add

--- src/com/example/androidkeyeventtest/MainActivity.java
+++ src/com/example/androidkeyeventtest/MainActivity.java
@@ -1,15 +1,64 @@
 package com.example.androidkeyeventtest;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private static final String TAG = "TestKeyEvent";
+	private ImageView mImage;
+	private TextView mAlphaValueText;
+	private int mAlphaValue;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mImage = (ImageView) findViewById(R.id.image);
+		mAlphaValueText = (TextView) findViewById(R.id.alphavalue);
+		mAlphaValue = 100;
+
+		mImage.setAlpha(mAlphaValue);
+		mAlphaValueText.setText("Alpha=" + mAlphaValue * 100 / 0xff + "%");
+
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent msg) {
+		// TODO Auto-generated method stub
+		Log.v(TAG, "KeyDown:keyCode=" + keyCode);
+		Log.v(TAG, "KeyDown:String=:" + msg.toString());
+
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_DPAD_UP:
+		case KeyEvent.KEYCODE_DPAD_RIGHT:
+			mAlphaValue += 20;
+			break;
+
+		case KeyEvent.KEYCODE_DPAD_DOWN:
+		case KeyEvent.KEYCODE_DPAD_LEFT:
+			mAlphaValue -= 20;
+			break;
+		default:
+			mAlphaValue += 20;
+			break;
+		}
+		
+		if(mAlphaValue>=0xFF) 
+			mAlphaValue=0xFF;
+		if(mAlphaValue<=0x0)
+			mAlphaValue=0x0;
+		mImage.setAlpha(mAlphaValue);
+		mAlphaValueText.setText("Alpha=" + mAlphaValue * 100 / 0xff + "%");
+		
+
+		return super.onKeyDown(keyCode, msg);
 	}
 
 	@Override
